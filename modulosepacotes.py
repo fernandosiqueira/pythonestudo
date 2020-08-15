74bd8c37a34bdf19dc0eb9057102e2ef5349289f
Usuário <fernando.gomes.siqueira@gmail.com>
2020-08-15 18:45:23 -0300

    15/08/20

---
+++ modulosepacotes.py
@@ -0,0 +1,10 @@
+from pacotes.poligono import Quadrado, Retangulo
+
+print('Quadrado')
+q = Quadrado(5)
+print(q.perimetro())
+print(q.area())
+
+print('Retangulo')
+w = Retangulo(6,7)
+print(w.area())
